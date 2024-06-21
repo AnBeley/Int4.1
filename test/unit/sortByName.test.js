50ed8cb6a2360b376900d1a7aba536d66b69c1f3
Anna Markova <anbeley@mail.ru>
2024-06-21 23:58:40 +0300

    first commit

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,57 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+  });
+
+  it("Shouldn't being sorted if descending order", () => {
+    const input = [
+      "Властелин Колец",
+      "Гарри Поттер",
+      "Дом, который пострил Джек",
+    ];
+
+    const expected = [
+      "Властелин Колец",
+      "Гарри Поттер",
+      "Дом, который пострил Джек",
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+  });
+
+  it("Books names should be sorted if duplicated", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
+  });
+});
